de7e5638d7c65705935ecafe7c5be9690239d929
tusmester <tusmester@gmail.com>
2017-07-10 13:40:05 +0200

    Merge changes from the release branch before release beta2.

---
+++ src/WebPages/Portlets/DialogFileUploadPortlet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SenseNet.Portal.UI.PortletFramework;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SenseNet.Portal.Portlets
+{
+    public class DialogFileUploadPortlet : ContextBoundPortlet
+    {
+        private const string DialogFileUploadPortletClass = "DialogFileUploadPortlet";
+        private const string  VIEWPATH = "/Root/System/SystemPlugins/Controls/DialogFileUpload.ascx";
+
+        [WebBrowsable(true), Personalizable(true)]
+        [LocalizedWebDisplayName(DialogFileUploadPortletClass, "Prop_AllowedContentTypes_DisplayName")]
+        [LocalizedWebDescription(DialogFileUploadPortletClass, "Prop_AllowedContentTypes_Description")]
+        [WebCategory("Dialog File Upload", 100)]
+        [WebOrder(100)]
+        public string AllowedContentTypes { get; set; }
+
+        public DialogFileUploadPortlet()
+        {
+            this.Name = "$DialogFileUploadPortlet:PortletDisplayName";
+            this.Description = "$DialogFileUploadPortlet:PortletDescription";
+            this.Category = new PortletCategory(PortletCategoryType.Application);
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            var control = this.Page.LoadControl(VIEWPATH) as UI.Controls.DialogFileUpload;
+            if (control != null)
+            {
+                control.AllowedContentTypes = this.AllowedContentTypes;
+                this.Controls.Add(control);
+            }
+
+            base.OnInit(e);
+        }
+    }
+}
