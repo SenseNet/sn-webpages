f523b6993cc9afbc5d1b22384703d76f3aed675e
tusmester <tusmester@gmail.com>
2017-08-22 13:36:32 +0200

    Add TaskMonitor feature.

---
+++ src/WebPages/Hubs/TaskMonitorHub.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Client;
+using SenseNet.BackgroundOperations;
+using SenseNet.ContentRepository;
+using SenseNet.Diagnostics;
+using SenseNet.TaskManagement.Core;
+using Task = System.Threading.Tasks.Task;
+
+namespace SenseNet.Portal.Hubs
+{
+    /// <summary>
+    /// A proxy class that routes every Sense/Net web client connection to the Task Management web app.
+    /// </summary>
+    [SenseNetAuthorizeHubAttribute(typeof(TaskMonitorHub))]
+    public class TaskMonitorHub : Hub
+    {
+        // a connection that we use to access the central Task Management web application
+        private static HubConnection _hubConnection;
+        private HubConnection TaskManagerHubConnection
+        {
+            get
+            {
+                if (_hubConnection == null)
+                {
+                    // build a connection to the TaskManagement web
+                    var appId = Settings.GetValue<string>(SnTaskManager.Settings.SETTINGSNAME, SnTaskManager.Settings.TASKMANAGEMENTAPPID);
+                    var querystringData = new Dictionary<string, string> { { "appid", appId } };
+                    var hubConnection = new HubConnection(Settings.GetValue<string>(SnTaskManager.Settings.SETTINGSNAME, SnTaskManager.Settings.TASKMANAGEMENTURL), querystringData);
+                    var taskManagerProxy = hubConnection.CreateHubProxy("TaskMonitorHub");
+
+                    // register client methods called by the TaskManagement web
+                    taskManagerProxy.On<string, SnHealthRecord>("Heartbeat", Heartbeat);
+                    taskManagerProxy.On<SnTaskEvent>("OnTaskEvent", OnTaskEvent);
+                    taskManagerProxy.On<SnProgressRecord>("WriteProgress", WriteProgress);
+
+                    StartConnection(hubConnection);
+
+                    hubConnection.Closed += () =>
+                    {
+                        SnTrace.System.Write("SNTaskMonitorHub connection to the task manager web app closed.");
+
+                        // reset static variables to let the system re-open the connection
+                        _hubConnection = null;
+                        _taskManagerProxy = null;
+                    };
+
+                    // store the connection objects
+                    _hubConnection = hubConnection;
+                    _taskManagerProxy = taskManagerProxy;
+                }
+
+                return _hubConnection;
+            }
+        }
+        
+        private static IHubProxy _taskManagerProxy;
+        private IHubProxy TaskManagerProxy
+        {
+            get
+            {
+                if (_taskManagerProxy == null)
+                {
+                    // create the connection and the proxy
+                    var hc = TaskManagerHubConnection;
+                }
+
+                return _taskManagerProxy;
+            }
+        }
+
+        // ===================================================================== Task Management Web Hub API (called by clients)
+
+        /// <summary>
+        /// Loads all tasks from the database that are registered, but not finished or failed. The real status of
+        /// currently in progress tasks will be set with the next progress or event call.
+        /// </summary>
+        /// <param name="appId">Application id to identify the client application.</param>
+        /// <param name="tag">If a tag is provided, events will be filtered by it.</param>
+        /// <returns></returns>
+        public SnTaskEvent[] GetUnfinishedTasks(string appId, string tag)
+        {
+            SnTrace.System.Write("SNTaskMonitorHub GetUnfinishedTasks. AppId: {0}", appId);
+
+            // make sure that the connection is alive
+            if (!CheckConnection())
+                return new SnTaskEvent[0];
+
+            // redirect the call to the task management web
+            return TaskManagerProxy.Invoke<SnTaskEvent[]>("GetUnfinishedTasks", appId, tag ?? string.Empty).Result;
+        }
+
+        /// <summary>
+        /// Loads all task and subtask events for a single task.
+        /// </summary>
+        /// <param name="appId">Application id to identify the client application.</param>
+        /// <param name="tag">If a tag is provided, events will be filtered by it.</param>
+        /// <param name="taskId">Id of the task to load events for.</param>
+        /// <returns></returns>
+        public SnTaskEvent[] GetDetailedTaskEvents(string appId, string tag, int taskId)
+        {
+            SnTrace.System.Write("SNTaskMonitorHub GetDetailedTaskEvents. AppId: {0}, TaskId: {1}", appId, taskId);
+
+            // make sure that the connection is alive
+            if (!CheckConnection())
+                return new SnTaskEvent[0];
+
+            // redirect the call to the task management web
+            return TaskManagerProxy.Invoke<SnTaskEvent[]>("GetDetailedTaskEvents", appId, tag ?? string.Empty, taskId).Result;
+        }
+
+        // ===================================================================== Client Hub API (called by the server)
+
+        /// <summary>
+        /// Periodically calles the Heartbeat client method for providing state information about task agents. The message is sent to all clients.
+        /// </summary>
+        public void Heartbeat(string agentName, SnHealthRecord healthRecord)
+        {
+            SnTrace.System.Write("SNTaskMonitorHub Heartbeat. AgentName: {0}, healthRecord: {1}", agentName, healthRecord);
+
+            Clients.All.Heartbeat(agentName, healthRecord);
+        }
+
+        /// <summary>
+        /// Calls the OnTaskEvent client method when a task state event occurs (e.g. started, finished, etc.). 
+        /// Only clients with the appropriate app id are called.
+        /// </summary>
+        public void OnTaskEvent(SnTaskEvent e)
+        {
+            SnTrace.System.Write("SNTaskMonitorHub OnTaskEvent: {0}, taskId: {1}, agent: {2}", e.EventType, e.TaskId, e.Agent);
+
+            Clients.Group(e.AppId).OnTaskEvent(e);
+        }
+
+        /// <summary>
+        /// Calls the WriteProgress client method when a subtask progress event occurs.
+        /// </summary>
+        public void WriteProgress(SnProgressRecord progressRecord)
+        {
+            SnTrace.System.Write("SNTaskMonitorHub WriteProgress: {0}, taskId: {1}", progressRecord.Progress.OverallProgress, progressRecord.TaskId);
+
+            Clients.Group(progressRecord.AppId).WriteProgress(progressRecord);
+        }
+
+        // ===================================================================== Overrides
+
+        public override async Task OnConnected()
+        {
+            // Make sure that the connection toward the server exists. If it doesn't,
+            // we have to throw an exception here otherwise clients of this hub 
+            // would remain connected and may think that everything is all right.
+            if (!CheckConnection())
+                throw new ApplicationException("TaskManager is unreachable.");
+
+            var expectedAppId = Settings.GetValue<string>(SnTaskManager.Settings.SETTINGSNAME, SnTaskManager.Settings.TASKMANAGEMENTAPPID);
+            var appid = Context.QueryString["appid"];
+            if (string.IsNullOrEmpty(appid) || string.Compare(appid, expectedAppId, StringComparison.InvariantCulture) != 0)
+            {
+                // unknown app
+                SnTrace.System.Write("SNTaskMonitorHub Client connected WITHOUT a correct app id.");
+                return;
+            }
+
+            // The appid is the same for every client, but we have to make sure that
+            // we call client methods only if they provided the same app id.
+            await Groups.Add(Context.ConnectionId, appid);
+
+            await base.OnConnected();
+
+            SnTrace.System.Write("SNTaskMonitorHub Client connected. ConnectionId: " + Context.ConnectionId);
+        }
+
+        // ===================================================================== Helpers
+
+        private static void StartConnection(HubConnection hubConnection)
+        {
+            try
+            {
+                hubConnection.Start().Wait();
+            }
+            catch (Exception ex)
+            {
+                SnTrace.System.Write("SNTaskMonitorHub connection error. " + ex);
+            }
+        }
+
+        private bool CheckConnection()
+        {
+            // reconnect if needed
+            if (TaskManagerHubConnection.State != ConnectionState.Connected)
+                StartConnection(TaskManagerHubConnection);
+
+            return TaskManagerHubConnection.State == ConnectionState.Connected;
+        }
+    }
+}
---
+++ src/nuget/snadmin/install-webpages/import/Global/plugins/BackgroundOperations/backgroundOperations.ascx
@@ -0,0 +1,74 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" %>
+<%@ Import Namespace="SenseNet.Portal.Virtualization" %>
+<%@ Import Namespace="SenseNet.BackgroundOperations" %>
+<%@ Import Namespace="SenseNet.ContentRepository" %>
+<%@ Import Namespace="SenseNet.ContentRepository.Storage.Data" %>
+<%@ Import Namespace="SenseNet.ContentRepository.i18n" %>
+<sn:ScriptRequest ID="respond" runat="server" Path="$skin/scripts/respond.js" />
+<sn:ScriptRequest ID="signalR" runat="server" Path="$skin/scripts/jquery/plugins/jquery.signalR-2.2.0.min.js" />
+<sn:ScriptRequest ID="Scriptrequest1" runat="server" Path="$skin/scripts/sn/SN.TaskMonitor.js" />
+<sn:ScriptRequest ID="hub" runat="server" Path="/signalr/hubs" />
+<sn:ScriptRequest ID="Scriptrequest2" runat="server" Path="$skin/scripts/kendoui/kendo.web.min.js" />
+<sn:CssRequest ID="kendocss1" runat="server" CSSPath="$skin/styles/kendoui/kendo.common.min.css" />
+<sn:CssRequest ID="kendocss2" runat="server" CSSPath="$skin/styles/kendoui/kendo.metro.min.css" />
+<sn:CssRequest ID="CssRequest0" runat="server" CSSPath="$skin/styles/SN.TaskMonitor.css" />
+<sn:CssRequest ID="CssRequest1" runat="server" CSSPath="$skin/styles/fontawesome/font-awesome.min.css" />
+
+<div id="taskmonitorWrapper">
+    <h1>Background Operations<span id="connectionState" class="sn-icon fa fa-plug disconnected" title='<%= SenseNetResourceManager.Current.GetString("$BackgroundOperations,Disconnected") %>'></span></h1>
+    <div id="taskDataGrid"></div>
+</div>
+
+
+<script type="text/x-kendo-template" id="machinesContainerTemplate">
+    <div id="machinesWrapper">
+        <span class="deselectAllMachines" data-bind="attr:{class: deselectClass}, click: deselectMachines"><%= SenseNetResourceManager.Current.GetString("$BackgroundOperations,showAllMachines") %></span>
+        <div data-bind="source: machinesSource" data-template="machineTemplate" id="machinesContainer"></div>
+    </div>
+</script>
+
+<script type="text/x-kendo-template" id="machineTemplate">
+    <div class="machine" data-bind="attr:{class: setClass}, click: selectMachine, style:{color: getColor, backgroundColor: getBackgroundColor}">
+        <div class="machineName" data-bind="text: Machine"></div>
+        <div class="indicator-container" data-bind="style:{borderColor:getColor}">
+            <p>CPU</p><span class="CPUContainer"><span class="indicator cpuIndicator" data-bind="style: {width: CPUPercent, backgroundColor: getColor}"></span></span><br />
+            <p>RAM</p><span class="RAMContainer"><span class="indicator ramIndicator" data-bind="style: {width: RAMPercent, backgroundColor: getColor}"></span></span>
+        </div>
+        <div class="machineDetails" data-bind="visible: isDetailsVisible">
+            <div style="text-align: left;">
+    <div class="detailsLabel">            
+    <span data-role="button" class="fa fa-close hideDetails" data-bind="click:hideDetails"></span>
+                <p>CPU usage:</p>
+                <span class="" data-bind="text: CPUPercent"></span> <br />
+                <p>Total RAM:</p>
+                <span class="" data-bind="text: TotalRamText"></span> <br />
+                <p>Used RAM:</p>
+                <span class="" data-bind="text: UsedRamText"></span> <br />
+    </div>
+                <div id="agentsContainer" data-bind="source: Agents" data-template="agentTemplate"></div>
+            </div>
+        </div>
+    </div>
+</script>
+
+<script type="text/x-kendo-template" id="agentTemplate">
+    <div class="fa fa-desktop agent" data-bind="attr: {title: getAgentName, data-agentId: getAgentId}, text: getAgentName, click: selectAgent, style:{color: getColor, backgroundColor: getBackgroundColor}"></div>
+</script>
+
+<script type="text/x-kendo-template" id="template">
+                <div class="toolbar">
+                    <label class="category-label" for="category"><%= SenseNetResourceManager.Current.GetString("$BackgroundOperations,filterByStatus") %></label>
+                    <input type="search" id="statusFilter" style="width: 150px"/>
+                </div>
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        var appId = '<%= Settings.GetValue<string>(TaskManager.Settings.SETTINGSNAME, TaskManager.Settings.TASKMANAGEMENTAPPID, null, "SenseNet") %>';
+
+        $("#taskDataGrid").SenseNetTaskGrid({
+            appId: appId
+        });
+    });
+</script>
