a64f107fee6ff461833c499100dd7bc08cde489f
tusmester <tusmester@gmail.com>
2017-09-05 14:49:02 +0200

    [KFI]fix: Move missing content items and classes from TFS.

---
+++ src/WebPages/ApplicationModel/PurgeFromProxyAction.cs
@@ -0,0 +1,20 @@
+﻿using SenseNet.Configuration;
+using SenseNet.ContentRepository;
+
+namespace SenseNet.ApplicationModel
+{
+    public class PurgeFromProxyAction : UrlAction
+    {
+        public override void Initialize(Content context, string backUri, Application application, object parameters)
+        {
+            base.Initialize(context, backUri, application, parameters);
+
+            if (context == null)
+                return;
+
+            // if there are no proxy servers defined, make this action forbidden
+            if (WebApplication.ProxyIPs.Count == 0)
+                this.Forbidden = true;
+        }
+    }
+}
--- src/WebPages/ApplicationModel/SetAsDefaultViewAction.cs
+++ src/WebPages/ApplicationModel/SetAsDefaultViewAction.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Web;
 using SenseNet.ContentRepository;
+using SenseNet.Portal.UI.ContentListViews.Handlers;
+using SenseNet.Portal.Virtualization;
 
 namespace SenseNet.ApplicationModel
 {
-    public class SetAsDefaultViewAction : UrlAction
+    public class SetAsDefaultViewAction : ODataActionBase
     {
+        public override string MethodName { get; set; } = "SetAsDefaultView";
+
+        // This is not exactly true, as it changes the default view on the content list - but we
+        // have to allow this action to be invoked as a simple GET request as a legacy feature.
+        public override bool CausesStateChange => false;
+        public override bool IsHtmlOperation => false;
+
         public override void Initialize(Content context, string backUri, Application application, object parameters)
         {
             base.Initialize(context, backUri, application, parameters);
 
-            var clv = context.ContentHandler as Portal.UI.ContentListViews.Handlers.ViewBase;
+            this.IncludeBackUrl = true;
+
+            var clv = context.ContentHandler as ViewBase;
             if (clv == null)
             {
                 this.Forbidden = true;
@@ -29,6 +41,26 @@ namespace SenseNet.ApplicationModel
             // if this view is the default, the action is meaningless
             if (string.Compare(cl.DefaultView, context.Name, StringComparison.InvariantCulture) == 0)
                 this.Forbidden = true;
+        }
+
+        public override object Execute(Content content, params object[] parameters)
+        {
+            var ctxView = content?.ContentHandler as ViewBase;
+            if (ctxView != null)
+            {
+                var list = ContentList.GetContentListByParentWalk(ctxView);
+                if (list != null)
+                {
+                    list.DefaultView = ctxView.Name;
+                    list.Save(SavingMode.KeepVersion);
+                }
+            }
+
+            var backUrl = PortalContext.Current.BackUrl;
+            var back = string.IsNullOrWhiteSpace(backUrl) ? "/" : backUrl;
+
+            HttpContext.Current.Response.Redirect(back, true);
+            return null;
         }
     }
 }
