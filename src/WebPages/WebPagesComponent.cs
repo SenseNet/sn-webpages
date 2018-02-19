6c638fb718b3c73dfe73ec5c6d259ac272b5fc7b
tusmester <tusmester@gmail.com>
2018-02-19 18:07:51 +0100

    Fix: WebPages component class is made public so that the system finds it.

--- src/WebPages/WebPagesComponent.cs
+++ src/WebPages/WebPagesComponent.cs
@@ -2,7 +2,7 @@
 
 namespace SenseNet.WebPages
 {
-    internal class WebPagesComponent : SnComponent
+    public class WebPagesComponent : SnComponent
     {
         public override string ComponentId => "SenseNet.WebPages";
     }
