34b958330674f70848079fb81675e988d87c0399
tusmester <tusmester@gmail.com>
2018-06-27 12:16:57 +0200

    Add Supported component version info.

--- src/WebPages/WebPagesComponent.cs
+++ src/WebPages/WebPagesComponent.cs
@@ -1,9 +1,11 @@
-﻿using SenseNet.ContentRepository;
+﻿using System;
+using SenseNet.ContentRepository;
 
 namespace SenseNet.WebPages
 {
     public class WebPagesComponent : SnComponent
     {
         public override string ComponentId => "SenseNet.WebPages";
+        public override Version SupportedVersion { get; } = new Version(7, 1, 0);
     }
 }
