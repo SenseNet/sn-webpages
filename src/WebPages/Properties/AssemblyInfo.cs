ce138859168c2ff4f2626c7db363b6088b4994df
SN\plastic <varga.yosh@gmail.com>
2019-10-08 11:14:44 +0200

    raise version to 7.3

--- src/WebPages/Properties/AssemblyInfo.cs
+++ src/WebPages/Properties/AssemblyInfo.cs
@@ -18,9 +18,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.2.4.1")]
-[assembly: AssemblyFileVersion("7.2.4.1")]
-[assembly: AssemblyInformationalVersion("7.2.4.1")]
+[assembly: AssemblyVersion("7.3.0")]
+[assembly: AssemblyFileVersion("7.3.0")]
+[assembly: AssemblyInformationalVersion("7.3.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7c7cd205-75ad-4c99-b745-9b1a6b724be9")]
